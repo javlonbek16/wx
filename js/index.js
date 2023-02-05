b95840a5f82826eea03b2b3269a5d70b39e4fa1e
javlonbek16 <nemaotovjavlonbek16@gmail.com>
2023-02-05 21:29:46 +0500

    another commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let contentUl = document.querySelector(".content");
-let url = `https://pokeapi.co/api/v2/pokemon`;
+// let url = `https://pokeapi.co/api/v2/pokemon`;
 let searchBtn = document.querySelector(".searchBtn")
 searchBtn.addEventListener("click", getData,)
 
@@ -115,6 +115,8 @@ const loadCard = (data) => {
 getPokemons(`${base_url}?limit=100`);
 
 
+//bookmark
+
 let leftWrapper = document.querySelector(".left-wrapper")
 let cartItems = document.createElement("div");
 leftWrapper.appendChild(cartItems);
@@ -135,7 +137,7 @@ function saveToLocal() {
 }
 
 
-
+let cartCount = document.querySelector(".cartCount")
 function renderPosts(array, parent) {
   parent.innerHTML = "";
 
@@ -154,47 +156,170 @@ function renderPosts(array, parent) {
 
 
 
-// function () {
+function handleRemove(data) {
+  let filtered = savedPosts.filter((item) => item.id !== data.id);
 
-// }
-
-
-// let sort = document.querySelector("#sort")
-// sort.addEventListener('change', (e) => {
-//   const sortedData = (readUsers()).sort((a, b) => {
-//     if (e.target.value === 'sortA') {
-//       return a.name.toLowerCase()[0] > b.name.toLowerCase()[0] ? 1 : -1
-//     }
-//     if (e.target.value === 'sortZ') {
-//       return b.name.toLowerCase()[0] > a.name.toLowerCase()[0] ? 1 : -1
-//     }
-//   })
-//   loadCard(sortedData)
-// })
+  savedPosts = filtered;
+  renderPosts(savedPosts, cartItems);
+  saveToLocal();
+}
 
 
 
+const select = document.getElementById("selectGross");
+const limite = 9;
+let long,
+  i = 0,
+  final = limite;
 
 
-// function sortList() {
+async function obtenerPokemons() {
+  try {
+    const respuesta = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
+    );
+    const objeto = await respuesta.json();
+    let res = objeto.result;
+    long = res.length;
+    // console.log(long);
+    for (i; i < final; i++) {
+      if (i == long) {
+        break;
+      } else {
+        obtenerInfoPokemon(res[i].url);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    console.error("Error");
+  }
+}
 
-// }
+
+
+async function obtenerPokemonsPorTipo() {
+  try {
+    const respuest = await fetch(
+      `https://pokeapi.co/api/v2/type/${select.value}`
+    );
+    const objet = await respuest.json();
+    let resultado = objet.pokemon;
+    long = resultado.length;
+    for (i; i < final; i++) {
+      if (i == long) {
+        break;
+      } else {
+        obtenerInfoPokemon(resultado[i].pokemon.url);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    console.error("Error  Pokemons ");
+  }
+}
 
 
 
-// function readUsers() {
-//   try {
-//     const response = fetch(url)
-//     const body = response.json()
-//     return body
-//   } catch (error) {
-//     errorMessage(error.message)
-//   }
-// }
+async function obtenerInfoPokemon(url) {
+  contentUl.innerHTML = "";
+  try {
+    const respuesta = await fetch(url);
+    const objeto = await respuesta.json();
+    crearCard(objeto);
+  } catch (error) {
+    console.error(error);
+    console.error("Error");
+  }
+}
 
 
 
-//bookmark
+
+function crearCard(objeto) {
+  let type = "";
+  objeto.types.forEach((e) => {
+    type += `<span class="type type-${e.type.name}">${e.type.name}</span>`;
+  });
+
+  let stat = "";
+  for (let i = 0; i < objeto.stats.length; i++) {
+    stat += `
+          <span class="stat">
+              ${objeto.stats[i].stat.name[0].toUpperCase() +
+      objeto.stats[i].stat.name.substring(1)
+      }
+              : ${objeto.stats[i].base_stat}
+          </span>
+      `;
+  }
+
+  let img = objeto.sprites.other.home.front_default;
+  if (img == null) {
+    img = "https://cdn-icons-png.flaticon.com/512/42/42901.png";
+  }
+
+  let card = `
+      <div class="card">
+          <div class="card-header" 
+          style="background-color:${[objeto.types[0].type.name]}">
+              <span class="header-title">
+                  ${objeto.name[0].toUpperCase() + objeto.name.substring(1)}
+              </span>
+              <div class="header-img">
+                  <img class="img" 
+                  src=${img} />
+              </div>
+          </div>
+          <div class="card-info">
+              <div class="info-types">
+                  ${type}
+              </div>
+              <div class="info-stats">
+           
+              </div>
+          </div>
+      </div>
+  `;
+  contentUl.innerHTML += card;
+}
+
+
+
+
+async function cargarCombo() {
+  try {
+    const respuesta = await fetch("https://pokeapi.co/api/v2/type");
+    const objeto = await respuesta.json();
+    const resultado = objeto.results;
+    let options = "";
+    for (let i = 0; i < resultado.length - 2; i++) {
+      options += `
+              <option value=${i + 1}>${resultado[i].name}</option>
+          `;
+    }
+    select.innerHTML += options;
+  } catch (error) {
+    console.error(error);
+    console.error("Error  Combo");
+  }
+}
+
+
+
+
+select.addEventListener("change", () => {
+  final = limite;
+  i = 0;
+  obtenerPokemonsPorTipo();
+  scroll(0, 0);
+});
+
+
+
+
+
+
+
 
 
 
